ac31b8fbcc0352177ae8302af4b67719b6150d4f
Ben Fawcett <ben.fawcett@nqry.com.au>
2021-02-22 02:46:34 +1030

    Removing credentials

--- src/scripts/scraper.ts
+++ src/scripts/scraper.ts
@@ -1,7 +1,16 @@
 import RedditApi from '../lib/RedditApi';
 import {DateTime} from 'luxon';
+import dotenv from 'dotenv';
+import path from 'path';
 
-const Api = new RedditApi("5mFnEMZ7fV5Cwg", "tchGBf1H31fFu8Vzuhz5uzrr257zPQ");
+dotenv.config({path: path.join(__dirname, "..", "..", ".env")});
+
+const Api = new RedditApi(process.env.REDDIT_CLIENT_ID!, process.env.REDDIT_CLIENT_SECRET!);
+console.log(process.env.REDDIT_CLIENT_ID)
+
+Api.search({
+    q: 'test'
+})
 
 const dateArray = [new DateTime(), new DateTime().minus({days: 1}), new DateTime().minus({days: 2})];
 
